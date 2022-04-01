a31f53cbff0fc45130fd5d802e8aa3dfa2122fbf
Sudo-Si <56238070+Sudo-Si@users.noreply.github.com>
2022-04-01 13:34:11 +1300

    heroku files finnished and fixed 3

--- app.js
+++ app.js
@@ -23,4 +23,8 @@ app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('mern_a_to_z_client/build'));
+}
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
