c2fdf3f165f3348594192f3ad9fa8891e00928b3
Othusitse <ootoothestudent@gmail.com>
2022-05-28 11:38:57 +1200

    searchbar feture works next to implement it into the main code to access the list of books in db

--- mern_a_to_z_client/src/components/AutoComplete.js
+++ mern_a_to_z_client/src/components/AutoComplete.js
@@ -1,21 +1,21 @@
 import React from "react";
-
-
-
-
+import '../AutoComplete.css';
 export default class AutoComplete extends React.Component{
- constructor(props){
-     super(props);
-     this.books =[
-        'How to code ',
-        'Code kings ',
-        'My little codie ',
-     ];
-     this.state = {
-        suggestions:[],
-     };
-}
-
+    constructor(props){
+        super(props);
+        this.books =[
+            'How to code ',
+            'Code kings ',
+            'Code kings vol iv ',
+            'Code kings vol v ',
+            'My little codie ',
+            'My little codie II ',
+        ];
+        this.state = {
+            suggestions:[],
+            text :'',
+        };
+    }   
  onTextChanged = (e) =>{
         const value = e.target.value;
         let suggestions =[];
@@ -24,31 +24,42 @@ export default class AutoComplete extends React.Component{
 
          suggestions = this.books.sort().filter(v =>regex.test(v));
          }
-         this.setState(()=>({suggestions}));
+         this.setState(()=>({suggestions, text : value}));
          
-         }   
+         }  
+         
+suggestionSelected(value){
+    this.setState(()=>({
+        text:value,
+        suggestions:[],
+
+    }))
+
+
+}
 //destructure suggestions
 renderSuggestions(){
-    const { suggestions }= this.state;
-    if(suggestions.length === 0){
-        
-        return null;
+        const { suggestions }= this.state;
+        if(suggestions.length === 0){
+            
+            return null;
+        }
+        return (
+            <ul>
+            {suggestions.map((book)=> <li onClick={()=> this.suggestionSelected(book)}>{book}</li>)}
+            </ul> 
+
+        );
     }
-    return (
-        <ul>
-        {suggestions.map((book)=> <li>{book}</li>)}
-        </ul> 
-
-    );
-}
     render(){
+
+        const {text}= this.state;
         return(
+            
 
-
-            <div> 
+            <div className="AutoComplete"> 
             <h1 >auto complete test</h1>
-          
-            <input onChange={this.onTextChanged} type= "text"/>
+            <input value={text} onChange={this.onTextChanged} type= "text"   placeholder='enter book name to search '/>
       
              {this.renderSuggestions()}
             
